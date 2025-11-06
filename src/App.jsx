c250a2321819c5f2128d10c669904134002fae80
GuilhermeZxx <guilhermeturkot08@gmail.com>
2025-11-06 14:53:04 -0300

    Corrigido conflitos e ajustes no App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,9 @@ export default function App(){
   return (
     <div>
       <h1>hello world!</h1>
-<<<<<<< HEAD
-    <h2>estamos testando</h2>
-=======
-    <h2>estamos aprendendo</h2>
->>>>>>> a47cc440a492da668e365b0c9fdaa2efd996dd99
-  
+
+      <h2>estamos testando</h2>
+
     </div>
     )
 }
